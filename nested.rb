20d2594caad1d20cf785e4efc92894f413e8899d
Robert M Ricci <rmricci121@gmail.com>
2019-10-26 11:09:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,7 @@ def dennis_ritchies_language
      }
      programmer_hash[:dennis_ritchie][:languages]="C"
      puts programmer_hash
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
