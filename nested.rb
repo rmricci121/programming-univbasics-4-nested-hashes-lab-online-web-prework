d281dadebc1799dd9d1774b50b0a455668d877f5
Robert M Ricci <rmricci121@gmail.com>
2019-10-26 11:21:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,6 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    adding_to_dennis[dennis_ritchie][languages] =+ "Assembly"
+    [dennis_ritchie][languages] =+ "Assembly"
     return adding_to_dennis
 end
